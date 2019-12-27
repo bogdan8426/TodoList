2233e26fcd283059b620ad69dc69b359d3d45039
Bogdan Rotaru <rotarubogdan96@gmail.com>
2019-12-27 16:47:36 +0200

    data binding and text wrapping

--- src/com/bogdanrotaru/todolist/Controller.java
+++ src/com/bogdanrotaru/todolist/Controller.java
@@ -44,7 +44,7 @@ public class Controller {
             }
         });
 
-        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+        todoListView.setItems(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
@@ -72,10 +72,7 @@ public class Controller {
         if(result.isPresent() && result.get() == ButtonType.OK){
             DialogController controller = fxmlLoader.getController();
             TodoItem newItem = controller.processResults();
-            todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
             todoListView.getSelectionModel().select(newItem);
-        }else{
-            System.out.println("Cancel pressed");
         }
 
     }
